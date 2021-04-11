db79d4b890accf436c2884f123469f49d6379edd
almazboot <76486953+almazboot@users.noreply.github.com>
2021-04-11 17:38:39 +0500

    Add files via upload

--- www/config.php
+++ www/config.php
@@ -5,6 +5,6 @@ define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
 define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
 define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
 
-define('CALLBACK_API_CONFIRMATION_TOKEN', 'bb112c41'); //Строка для подтверждения адреса сервера из настроек Callback API
-define('VK_API_ACCESS_TOKEN', '016cb55230eb83e34f620389s149345d975bc12e2fb6f30e0a833a51d345d0d123c3dc0abc1c864036sdf989fb8345'); //Ключ доступа сообщества
-define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
+define('CALLBACK_API_CONFIRMATION_TOKEN', 'ftqglrcnmdot57q6b0cs08r4v8d7g8p3o'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', '057d39af9b089f5f32a02f049af4a4c98746e16067dd44f8b560d78a5308f28e209d1b6883277b93582a2'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', 'b1gvn2m7rsblg0gappis'); //Ключ для доступа к Yandex Speech Kit
